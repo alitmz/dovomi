86e3584cfda945f4c90f8e066d071880c0877ba6
alitmz <80671189+alitmz@users.noreply.github.com>
2021-06-25 12:46:21 +0430

    hichi
    bebin

---
+++ Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	unsigned int tedad;
+	int n, m, k;
+	cin >> tedad;
+	while (tedad--) {
+		cin >> n >> m >> k;
+		if (((n - 1) * m + (m - 1)) == k)
+			cout << "YES\n";
+		else cout << "NO\n";
+
+	}
+
+	return 0;
+}
